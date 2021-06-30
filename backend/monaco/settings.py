984ac65835b01d864e6773b83dccbc705308f328
DJSull93 <digi809@naver.com>
2021-06-30 18:31:03 +0900

    rooo

--- backend/monaco/settings.py
+++ backend/monaco/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_*x9t4wf^cnja)yg*#^ro7u8#r7jj_hiz@39%fu0!ryeb79r#i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
